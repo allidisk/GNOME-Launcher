ff014150fcbabdaa7e7bdb6f3fc2e749a1f5c207
allidisk <j.mazerolle04@gmail.com>
2026-05-29 20:38:20 -0300

    Fix app scanning for all Android versions

--- app/src/main/java/com/gnome/launcher/data/AppRepository.kt
+++ app/src/main/java/com/gnome/launcher/data/AppRepository.kt
@@ -17,7 +17,7 @@ class AppRepository(private val context: Context) {
             addCategory(Intent.CATEGORY_LAUNCHER)
         }
 
-        val resolveInfoList: List<ResolveInfo> = pm.queryIntentActivities(intent, 0)
+        val resolveInfoList: List<ResolveInfo> = pm.queryIntentActivities(intent, PackageManager.MATCH_ALL)
 
         val apps = resolveInfoList
             .map { info ->
